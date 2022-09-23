dbf98928e2054553f16bbad03c3917cf611f1f3e
Davi Queiroz <dvbarreira@hotmail.com>
2022-09-23 20:08:07 -0300

    [FIX] Desquebrando o sistema

--- back/src/server.js
+++ back/src/server.js
@@ -6,12 +6,11 @@ const BitrixResource = require('./resource/bitrix.resource')
 const port = 4000
 
 app.use(cors())
-app.use(express.urlencoded( {extended: true} ))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
 
 app.get('/get-url-auth', BitrixResource.getUrlAuth)
 
 app.listen(port, () => {
-    console.info(`---- API funcionando na porta ${port} -----`)
+	console.info(`---- API funcionando na porta ${port} -----`)
 })
