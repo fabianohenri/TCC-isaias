d773869b9f3014048e5ba33410e850602f7366eb
Davi Queiroz <dvbarreira@hotmail.com>
2023-10-10 08:15:12 -0300

    [REFACTOR] Refatorando o modo que os gráficos são renderizados

--- front/src/pages/Dashboard/modules/GraphsModule/GraphsModule.jsx
+++ front/src/pages/Dashboard/modules/GraphsModule/GraphsModule.jsx
@@ -6,17 +6,90 @@ import GraphContainer from 'components/GraphContainer/GraphContainer'
 
 const graphHeight = 400
 const containerHeight = graphHeight + 80
+const allGraphColors = [
+	'#008FFB',
+	'#00E396',
+	'#FEB019',
+	'#FF4560',
+	'#964B00',
+	'#6A3D9A',
+	'#009CDF',
+	'#C9A76A',
+	'#EA526F',
+	'#4E3D63',
+	'#00A08B',
+	'#D8BF56',
+	'#957DAD',
+	'#738678',
+	'#6B4226',
+	'#984B43',
+	'#D2D4DC',
+	'#5E6A71',
+	'#1C0C1F',
+	'#14A7D0'
+]
 
 const GraphsModule = ({ data, loading }) => {
 	const [metrics, setMetrics] = useState({})
 
-	let canLoadAuditors = metrics?.usersGraphData?.auditors?.series?.length > 0
-	let canLoadCreators = metrics?.usersGraphData?.creators?.series?.length > 0
-	let canLoadResponsibles = metrics?.usersGraphData?.responsibles?.series?.length > 0
-	let canLoadClosers = metrics?.usersGraphData?.closers?.series?.length > 0
-	let canLoadAccomplices = metrics?.usersGraphData?.accomplices?.series?.length > 0
-	let canLoadTagsPopular = metrics?.tagsGraphData?.popular?.series?.length > 0
-	let canLoadAverageMemberCompletionTime = metrics?.completionGraphData?.averageTime?.series?.length > 0
+	const graphsToRender = [
+		{
+			canLoad: metrics?.tagsGraphData?.popular?.series?.length > 0,
+			series: metrics?.tagsGraphData?.popular?.series,
+			labels: metrics?.tagsGraphData?.popular?.labels,
+			title: 'Tags',
+			additionalOptions: { formatterLabel: ' tarefas' },
+			xs: 6
+		},
+		{
+			canLoad: metrics?.completionGraphData?.averageTime?.series?.length > 0,
+			series: metrics?.completionGraphData?.averageTime?.series,
+			labels: metrics?.completionGraphData?.averageTime?.labels,
+			title: 'Média de finalização de tarefas',
+			additionalOptions: { formatterLabel: ' h' },
+			xs: 6
+		},
+		{
+			canLoad: metrics?.usersGraphData?.responsibles?.series?.length > 0,
+			series: metrics?.usersGraphData?.responsibles?.series,
+			labels: metrics?.usersGraphData?.responsibles?.labels,
+			title: 'Responsáveis',
+			additionalOptions: { formatterLabel: ' tarefas' },
+			xs: 6
+		},
+		{
+			canLoad: metrics?.usersGraphData?.creators?.series?.length > 0,
+			series: metrics?.usersGraphData?.creators?.series,
+			labels: metrics?.usersGraphData?.creators?.labels,
+			title: 'Criadores',
+			additionalOptions: { formatterLabel: ' tarefas' },
+			xs: 6
+		},
+		{
+			canLoad: metrics?.usersGraphData?.closers?.series?.length > 0,
+			series: metrics?.usersGraphData?.closers?.series,
+			labels: metrics?.usersGraphData?.closers?.labels,
+			title: 'Fechadores',
+			additionalOptions: { formatterLabel: ' tarefas' },
+			xs: 6
+		},
+		{
+			canLoad: metrics?.usersGraphData?.accomplices?.series?.length > 0,
+			series: metrics?.usersGraphData?.accomplices?.series,
+			labels: metrics?.usersGraphData?.accomplices?.labels,
+			title: 'Participantes',
+			additionalOptions: { formatterLabel: ' tarefas' },
+			xs: 6
+		},
+		{
+			canLoad: metrics?.usersGraphData?.auditors?.series?.length > 0,
+			series: metrics?.usersGraphData?.auditors?.series,
+			labels: metrics?.usersGraphData?.auditors?.labels,
+			title: 'Observadores',
+			additionalOptions: { formatterLabel: ' tarefas' },
+			xs: 12
+		}
+	]
 
 	useEffect(() => {
 		if (data) {
@@ -26,113 +99,22 @@ const GraphsModule = ({ data, loading }) => {
 
 	return (
 		<Grid container>
-			<Grid item xs={6}>
-				<GraphContainer loading={loading} height={containerHeight} title='Tags'>
-					{canLoadTagsPopular && (
-						<BarChart
-							series={metrics?.tagsGraphData?.popular?.series}
-							labels={metrics?.tagsGraphData?.popular?.labels}
-							height={graphHeight}
-							width='100%'
-							colors={['#008FFB', '#00E396', '#FEB019', '#FF4560']}
-							isHorizontal={false}
-							isStacked={false}
-						/>
-					)}
-				</GraphContainer>
-			</Grid>
-			<Grid item xs={6}>
-				<GraphContainer loading={loading} height={containerHeight} title='Média de finalização de tarefas'>
-					{canLoadAverageMemberCompletionTime && (
-						<BarChart
-							series={metrics?.completionGraphData?.averageTime?.series}
-							labels={metrics?.completionGraphData?.averageTime?.labels}
-							additionalOptions={{ formatterLabel: ' h' }}
-							height={graphHeight}
-							width='100%'
-							colors={['#008FFB', '#00E396', '#FEB019', '#FF4560']}
-							isHorizontal={false}
-							isStacked={false}
-						/>
-					)}
-				</GraphContainer>
-			</Grid>
-			<Grid item xs={6}>
-				<GraphContainer loading={loading} height={containerHeight} xs={6} title='Responsáveis'>
-					{canLoadResponsibles && (
-						<BarChart
-							series={metrics?.usersGraphData?.responsibles?.series}
-							labels={metrics?.usersGraphData?.responsibles?.labels}
-							height={graphHeight}
-							width='100%'
-							colors={['#008FFB', '#00E396', '#FEB019', '#FF4560']}
-							isHorizontal={false}
-							isStacked={false}
-						/>
-					)}
-				</GraphContainer>
-			</Grid>
-			<Grid item xs={6}>
-				<GraphContainer loading={loading} height={containerHeight} title='Criadores'>
-					{canLoadCreators && (
-						<BarChart
-							series={metrics?.usersGraphData?.creators?.series}
-							labels={metrics?.usersGraphData?.creators?.labels}
-							height={graphHeight}
-							width='100%'
-							colors={['#008FFB', '#00E396', '#FEB019', '#FF4560']}
-							isHorizontal={false}
-							isStacked={false}
-						/>
-					)}
-				</GraphContainer>
-			</Grid>
-			<Grid item xs={6}>
-				<GraphContainer loading={loading} height={containerHeight} title='Fechadores'>
-					{canLoadClosers && (
-						<BarChart
-							series={metrics?.usersGraphData?.closers?.series}
-							labels={metrics?.usersGraphData?.closers?.labels}
-							height={graphHeight}
-							width='100%'
-							colors={['#008FFB', '#00E396', '#FEB019', '#FF4560']}
-							isHorizontal={false}
-							isStacked={false}
-						/>
-					)}
-				</GraphContainer>
-			</Grid>
-			<Grid item xs={6}>
-				<GraphContainer loading={loading} height={containerHeight} title='Participantes'>
-					{canLoadAccomplices && (
-						<BarChart
-							series={metrics?.usersGraphData?.accomplices?.series}
-							labels={metrics?.usersGraphData?.accomplices?.labels}
-							height={graphHeight}
-							width='100%'
-							colors={['#008FFB', '#00E396', '#FEB019', '#FF4560']}
-							isHorizontal={false}
-							isStacked={false}
-						/>
-					)}
-				</GraphContainer>
-			</Grid>
-
-			<Grid item xs={12}>
-				<GraphContainer loading={loading} height={containerHeight} title='Observadores'>
-					{canLoadAuditors && (
-						<BarChart
-							series={metrics?.usersGraphData?.auditors?.series}
-							labels={metrics?.usersGraphData?.auditors?.labels}
-							height={graphHeight}
-							width='100%'
-							colors={['#008FFB', '#00E396', '#FEB019', '#FF4560']}
-							isHorizontal={false}
-							isStacked={false}
-						/>
-					)}
-				</GraphContainer>
-			</Grid>
+			{graphsToRender.map((graph) => (
+				<Grid item xs={graph.xs} key={graph.title}>
+					<GraphContainer loading={loading} height={containerHeight} title={graph.title}>
+						{graph.canLoad && (
+							<BarChart
+								series={graph?.series}
+								labels={graph?.labels}
+								height={graphHeight}
+								width='100%'
+								colors={allGraphColors}
+								additionalOptions={graph.additionalOptions}
+							/>
+						)}
+					</GraphContainer>
+				</Grid>
+			))}
 		</Grid>
 	)
 }
