89864914051dcfb331ba125eec2f603c2b7a7b02
fabianohenri <fabiano_henri@yahoo.com.br>
2024-10-07 17:13:21 -0300

    Teste manual.

--- back/src/config.js
+++ back/src/config.js
@@ -1,23 +1,31 @@
 const DB = {
-	user: process.env.DATABASE_USER,
-	host: process.env.DATABASE_HOST,
-	database: process.env.DATABASE_NAME,
-	password: process.env.DATABASE_PASSWORD,
+	// user: process.env.DATABASE_USER,
+	// host: process.env.DATABASE_HOST,
+	// database: process.env.DATABASE_NAME,
+	// password: process.env.DATABASE_PASSWORD,
+	user: 'postgres',
+	host: 'infra-pg-15-svc.databases.svc.cluster.local',
+	database: 'graficos-bitrix',
+	password: 'rmURC0F76NH2',
 	port: 5432,
 	ssl: false,
 	connectionTimeoutMillis: 15000
 }
 
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+const JWT_SECRET_KEY = '99999a9999aAA'
 
 const BITRIX = {
-	CLIENT_ID: process.env.BITRIX_CLIENT_ID,
-	CLIENT_SECRET: process.env.BITRIX_CLIENT_SECRET,
-	URL: process.env.BITRIX_URL
+	// CLIENT_ID: process.env.BITRIX_CLIENT_ID,
+	// CLIENT_SECRET: process.env.BITRIX_CLIENT_SECRET,
+	// URL: process.env.BITRIX_URL
+	CLIENT_ID: 'local.66fb0e70d99067.57999783',
+	CLIENT_SECRET: 'amhQjfQZiAUmB5cS0N1X2rcZpIfCzVtWjLS8Msk8gHVky1APSF',
+	URL: 'bitrix24.com.br'
 }
 
 const ETC = {
-	BASE_FRONT_URL: process.env.BASE_FRONT_URL,
+	BASE_FRONT_URL: 'https://grafico-bitrix.projetusti.com.br',
 	KEEP_CONNECTED_HOURS: 720 //30 dias
 }
 
